05588f2c80054e050aa72e2e8b364d4edf83e9dd
Jaime Caamaño Ruiz <jcaamano@redhat.com>
2024-10-23 18:42:56 +0200

    More changes
    Signed-off-by: Jaime Caamaño Ruiz <jcaamano@redhat.com>

--- hack/demo/demo.sh
+++ hack/demo/demo.sh
@@ -12,23 +12,22 @@ NODES=$(kubectl get nodes -o jsonpath={.items[*].status.addresses[?\(@.type==\"I
 echo "Nodes IPs are: $NODES"
 
 for node in $NODES; do
-    FRR_IP=$(ip -j -d route get $node | jq -r '.[] | .dev' | xargs ip -d -j address show | jq -r '.[] | .addr_info[0].local')
-    NETWORK=$(docker network ls -f 'driver=bridge' -q | xargs docker network inspect | jq -r '.[] | select(any(.IPAM.Config[]; .Gateway=='"$gw"')) | .Name')
-    if [ -n "$NETWORK" ]; then
-        # assume container network
-	FRR_IP=$(sudo docker inspect -f "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" frr)
-    else
+    GW=$(ip -j -d route get $node | jq -r '.[] | .dev' | xargs ip -d -j address show | jq -r '.[] | .addr_info[0].local')
+    NETWORK=$($CLI network ls -f 'driver=bridge' -q | xargs $CLI network inspect | jq -r 'try .[] | select(any(.IPAM.Config[]; .Gateway=="'"$GW"'")) | .Name')
+    if [ -z "$NETWORK" ]; then
         # assume libvirt
 	NETWORK=host
     fi
     break
 done
 
-echo "FRR IP is: $FRR_IP"
-echo "NETWORK is: $NETWORK"
-
 pushd ./frr/
-go run . -frr "$FRR_IP" -nodes "$NODES"
+if [ "$NETWORK" = "host" ]; then
+    FRR_IP=$GW
+    go run . -frr "$FRR_IP" -nodes "$NODES"
+else
+    go run . -nodes "$NODES"
+fi
 popd
 
 FRR_CONFIG=$(mktemp -d -t frr-XXXXXXXXXX)
@@ -36,11 +35,15 @@ cp frr/*.conf $FRR_CONFIG
 cp frr/daemons $FRR_CONFIG
 chmod a+rwx -R $FRR_CONFIG
 
-sudo docker rm -f frr
-sudo docker run -d --privileged --network host --rm --ulimit core=-1 --name frr --volume "$FRR_CONFIG":/etc/frr quay.io/frrouting/frr:9.1.0
+sudo $CLI rm -f frr
+sudo $CLI run -d --privileged --network host --rm --ulimit core=-1 --name frr --volume "$FRR_CONFIG":/etc/frr quay.io/frrouting/frr:9.1.0
+
+if [ "$NETWORK" != "host" ]; then
+    FRR_IP=$(sudo $CLI inspect -f "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" frr)
+fi
 
 for i in configs/*.yaml; do
-	rm $i
+    rm $i
 done
 
 cp configs/templates/*.yaml configs/
@@ -49,4 +52,6 @@ for i in configs/*.yaml; do
     sed -i "s/NEIGHBOR_IP/$FRR_IP/g" $i
 done
 
+echo "FRR IP is: $FRR_IP"
+echo "NETWORK is: $NETWORK"
 echo "Setup is complete, demo yamls can be found in $(pwd)/configs"
